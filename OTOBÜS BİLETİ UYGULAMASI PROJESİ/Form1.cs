12b1e9b3f1f9af2b48f088efd003195c5d535737
Imran Imtiaz <imranimtiaz48@hotmail.com>
2024-07-16 08:47:31 +0400

    Update Form1.cs
    This version maintains the functionality of your original code while organizing it to be more readable and adherent to standard C# coding practices.

--- OTOBÜS BİLETİ UYGULAMASI PROJESİ/Form1.cs
+++ OTOBÜS BİLETİ UYGULAMASI PROJESİ/Form1.cs
@@ -1,4 +1,4 @@
-﻿using OTOBÜS_BİLETİ_UYGULAMASI_PROJESİ.Entities.Concrete;
+using OTOBÜS_BİLETİ_UYGULAMASI_PROJESİ.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,13 +13,13 @@ namespace OTOBÜS_BİLETİ_UYGULAMASI_PROJESİ
 {
     public partial class Form1 : Form
     {
+        private AppMember appMember = new AppMember();
+        private Button tiklanan;
+
         public Form1()
         {
             InitializeComponent();
         }
-
-
-        AppMember appMember = new AppMember();
 
         private void cmbOtobus_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -34,45 +34,31 @@ namespace OTOBÜS_BİLETİ_UYGULAMASI_PROJESİ
                 case "Pamukkale":
                     KoltukDoldur(8);
                     break;
-                
                 default:
                     break;
             }
         }
-        void KoltukDoldur(int sira)
+
+        private void KoltukDoldur(int sira)
         {
-            yavaslat:
-            foreach (Control ctrl in this.Controls)
-            {
-                if (ctrl is Button)
-                {
-                    Button btn = ctrl as Button;
-                    if (btn.Text == "Add")
-                    {
-                        continue;
-                    }else
-                    {
-                        this.Controls.Remove(ctrl);
-                        goto yavaslat;
-                    }
-                }
-
-            }
-
-
+            CleanUpButtons();
 
             int koltukNo = 1;
             for (int satir = 0; satir < sira; satir++)
             {
                 for (int sütun = 0; sütun < 4; sütun++)
                 {
-                    if (satir==3&&sütun>1)
+                    if (satir == 3 && sütun > 1)
                     {
                         continue;
                     }
-                    if (sütun == 1) continue;
+                    if (sütun == 1)
+                    {
+                        continue;
+                    }
+
                     Button koltuk = new Button();
-                    koltuk.Height = koltuk.Width=40;
+                    koltuk.Height = koltuk.Width = 40;
                     koltuk.Top = 30 + (satir * 45);
                     koltuk.Left = 100 + (sütun * 45);
                     koltuk.Text = koltukNo.ToString();
@@ -80,75 +66,60 @@ namespace OTOBÜS_BİLETİ_UYGULAMASI_PROJESİ
                     koltuk.ContextMenuStrip = contextMenuStrip1;
                     koltuk.MouseDown += koltuk_mouseDown;
                     this.Controls.Add(koltuk);
-
                 }
-
             }
         }
-        Button tiklanan;
+
+        private void CleanUpButtons()
+        {
+            foreach (Control ctrl in this.Controls.OfType<Button>().Where(btn => btn.Text != "Add").ToList())
+            {
+                this.Controls.Remove(ctrl);
+            }
+        }
 
         private void koltuk_mouseDown(object sender, MouseEventArgs e)
         {
             tiklanan = sender as Button;
-         
-        }
-
-        /*  private void koltuk_Click(object sender, EventArgs e)  
-         *  button eventi olusturduk tıkladıgında hangi button a tıkladıgını anlamasını sagladık.
-          {
-              Button btn = sender as Button;
-              MessageBox.Show(btn.Text);
-          }*/
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
 
         private void rezervasyonYapToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (cmbOtobus.SelectedIndex==-1||cmbNerden.SelectedIndex==-1||cmbNereye.SelectedIndex==-1)
+            if (cmbOtobus.SelectedIndex == -1 || cmbNerden.SelectedIndex == -1 || cmbNereye.SelectedIndex == -1)
             {
-                MessageBox.Show("Lüften Gerekli alanları doldurun");
+                MessageBox.Show("Lütfen gerekli alanları doldurun.");
                 return;
-
             }
+
             KayitFormu kf = new KayitFormu();
-            DialogResult sonuc=kf.ShowDialog();
+            DialogResult sonuc = kf.ShowDialog();
+
             if (sonuc == DialogResult.OK)
             {
                 ListViewItem lvi = new ListViewItem();
                 lvi.Text = string.Format("{0} {1}", kf.txtFirstname.Text, kf.txtLastname.Text);
                 lvi.SubItems.Add(kf.mskPhoneNumber.Text);
+
                 if (kf.rdeMan.Checked)
                 {
                     tiklanan.BackColor = Color.Blue;
                     lvi.SubItems.Add("MAN");
                 }
-                if (kf.rdWomen.Checked)
+                else if (kf.rdWomen.Checked)
                 {
+                    tiklanan.BackColor = Color.Pink;
                     lvi.SubItems.Add("WOMEN");
-                    tiklanan.BackColor = Color.Pink; 
                 }
+
                 lvi.SubItems.Add(cmbNerden.Text);
                 lvi.SubItems.Add(cmbNereye.Text);
                 lvi.SubItems.Add(tiklanan.Text);
                 lvi.SubItems.Add(dtpTarih.Text);
                 lvi.SubItems.Add(comboBox3.Text);
                 lvi.SubItems.Add(numUpFiyat.Value.ToString());
+
                 listView1.Items.Add(lvi);
-                        
-
             }
-
-            
-
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
